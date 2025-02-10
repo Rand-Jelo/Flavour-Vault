f26467e4e999be67701b63bb4faf27527d78654b
ClamiestGalaxy <162509575+ClamiestGalaxy@users.noreply.github.com>
2025-02-10 07:27:00 +0100

    Add am i responsive to allowed hosts to try to make the website access flavour vault

--- flavourvault/settings.py
+++ flavourvault/settings.py
@@ -33,10 +33,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '.herokuapp.com']
+ALLOWED_HOSTS = ['*', '.herokuapp.com', 'https://ui.dev/amiresponsive']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-randjelo-flavourvault-60crf5myf7p.ws.codeinstitute-ide.net'
+    'https://8000-randjelo-flavourvault-60crf5myf7p.ws.codeinstitute-ide.net',
+    'https://flavourvault.herokuapp.com',
+    'https://ui.dev/amiresponsive'
 ]
 
 
